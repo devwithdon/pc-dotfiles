51784a2c7e74680f157ea77aad8b176b4be20565
don <33989414+devwithdon@users.noreply.github.com>
2023-12-05 12:03:10 -0600

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -2,3 +2,9 @@ oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/catppuccin_mocha.omp.json" 
 
 function list { Get-ChildItem $args  | Format-Wide Name -AutoSize }
 Set-Alias -Name "ls" -Value "list"
+
+function runctt { Invoke-WebRequest -useb https://christitus.com/win | Invoke-Expression }
+Set-Alias -Name "ctt" -Value "runctt"
+
+function winup { winget upgrade --all }
+Set-Alias -Name "wua" -Value "winup"
